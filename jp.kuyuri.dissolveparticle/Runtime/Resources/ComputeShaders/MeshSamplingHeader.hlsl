2c42fb61a7ea5697f24ae5e9bb7cf427e8a6e55d
kuyuri-iroha <kuyuri.iroha@gmail.com>
2023-11-13 00:02:34 +0900

    メッシュ毎のディゾルブに対応するための改修

--- jp.kuyuri.dissolveparticle/Runtime/Resources/ComputeShaders/MeshSamplingHeader.hlsl
+++ jp.kuyuri.dissolveparticle/Runtime/Resources/ComputeShaders/MeshSamplingHeader.hlsl
@@ -7,9 +7,20 @@ struct MeshSamplingPoint
     float3 positionOS;
     float3 normal;
     float3 velocity;
+    uint meshIndex;
     float2 uv;
 };
 
-#define WORLD_TO_LOCAL_TRANSFORM_MAX 64
+struct SkinnedMeshData
+{
+    int meshIndex;
+    int materialCount;
+            
+    float4x4 currentRootMatrix;
+    float4x4 previousRootMatrix;
+    float4x4 worldToLocalTransform;
+};
+
+#define MESH_MAX 256
 
 #endif
